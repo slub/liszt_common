9d053abd84fb2727b9178ea9ce7ad10987fa2a2b
thomas-sc <info@schefter.net>
2024-03-25 13:36:10 +0100

    fix action controller for v13, include typoscript config files in sys_template, change Searchbar template

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -3,11 +3,13 @@
 declare(strict_types=1);
 
 namespace Slub\LisztCommon\Controller;
+use Psr\Http\Message\ResponseInterface;
 
 class SearchController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
-    public function indexAction(): void
+    public function indexAction(): ResponseInterface
     {
         $this->view->assign('foo', 'bar');
+        return $this->htmlResponse();
     }
 }
--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,11 +1,9 @@
 <?php
-/*defined('TYPO3') or die('Access denied.');
-call_user_func(function()
-{
+
+defined('TYPO3') or die();
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         'liszt_common',
         'Configuration/TypoScript',
         'Liszt-Common'
-    );
-});*/
+);
