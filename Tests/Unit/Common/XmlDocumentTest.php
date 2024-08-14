de22b87e557e941ad1a0f9ff19bb99077cfab906
Nathanael Mehlhorn <nathanael.georg.mehlhorn@slub-dresden.de>
2024-08-14 11:43:37 +0200

    Added more testes

--- Tests/Unit/Common/XmlDocumentTest.php
+++ Tests/Unit/Common/XmlDocumentTest.php
@@ -19,7 +19,6 @@ final class XmlDocumentTest extends UnitTestCase
 
         $this->xmlString = file_get_contents('Tests/Testfiles/meitest2.xml');
         $this->subject = XmlDocument::from($this->xmlString);
-
     }
 
     /**
@@ -41,29 +40,63 @@ final class XmlDocumentTest extends UnitTestCase
     /**
      * @test
      */
-
     public function xmlStringNotEmpty(): void
     {
         self::assertNotSame('', $this->xmlString);
     }
 
     /**
-    * @test
-    **/
+     * @test
+     */
+    public function writeTestFile()
+    {
+        $file = fopen('Tests/Testfiles/testout.json', 'w');
+        fwrite($file, $this->subject->setSplitSymbols(['meiHead'])->setXmlId(false)->toJson());
 
-    public function writeTestFile() {
-        $file = fopen("Tests/Testfiles/testout.json","w");
-        fwrite($file,$this->subject->setSplitSymbols(["meiHead"])->setXmlId(false)->toJson());
-        
-        self::assertFileExists("Tests/Testfiles/testout.json");
+        self::assertFileExists('Tests/Testfiles/testout.json');
     }
 
     /**
-    * @test
-    **/
-
-    public function testFluidInterface() {
+     * @test
+     */
+    public function testFluidInterface()
+    {
         self::assertJson($this->subject->setXmlId(true)->toJson());
-    }    
-}
+    }
 
+    /**
+     * @test
+     */
+    public function testXmlIdIsOmited()
+    {
+        $this->subject->setXmlId(false);
+        self::assertFalse(str_contains($this->subject->toJson(), '@xmlId'));
+    }
+
+    /**
+     * @test
+     */
+    public function testConvertedAttribute()
+    {
+        $subject = XmlDocument::from('<item id="1" name="ExampleItem" />');
+
+        $expected = '{
+        "@attributes": {
+            "id": "1",
+            "name": "ExampleItem"
+        }
+    }';
+
+        self::assertSame(json_decode($expected, true), json_decode($subject->toJson(), true));
+    }
+
+    /**
+     * @test
+     */
+    public function testPlainText()
+    {
+        $subject = XmlDocument::from('<p>I am plain text</p>');
+        $expected = '{"@value": "I am plain text"}';
+        self::assertSame(json_decode($subject->toJson(), true), json_decode($expected, true));
+    }
+}
