a9cb072e4796f7cdfba1b6873f611c66b00968ba
thomas-sc <info@schefter.net>
2024-10-09 15:50:28 +0200

    get Query Param text in Searchbar todo (caching Problem)

--- Classes/ViewHelpers/GetQueryParamsViewHelper.php
+++ Classes/ViewHelpers/GetQueryParamsViewHelper.php
@@ -1,28 +1,29 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Slub\LisztCommon\ViewHelpers;
 
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Type\File\ImageInfo;
-
-
 
 final class GetQueryParamsViewHelper extends AbstractViewHelper
 {
-
-
-    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): array
+    public function initializeArguments(): void
     {
+        $this->registerArgument('param', 'string', 'Name of the parameter key', true);
+    }
 
-        /** @var RenderingContext $renderingContext */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): ?string
+    {
         $request = $renderingContext->getRequest();
+        $queryParams = $request->getQueryParams();
 
+        // Check if the argument param exists in the query parameters
+        if (array_key_exists($arguments['param'], $queryParams)) {
+            return $queryParams[$arguments['param']];
+        }
 
-        return $request->getQueryParams();
-
+        // Return null if parameter does not exist
+        return null;
     }
 }
