bd4ba9fba06722e61453e4942121eb3684da7da0
thomas-sc <info@schefter.net>
2025-02-25 12:35:32 +0100

    Add plugin and action for header, runtime caching detail request in controller, view transitions

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -14,67 +14,64 @@ use Elastic\Elasticsearch\Exception\ClientResponseException;
 use Elastic\Transport\Exception\RuntimeException;
 use TYPO3\CMS\Core\MetaTag\MetaTagManagerRegistry;
 use Slub\LisztCommon\Common\PageTitleProvider;
-// ToDo:
-// Organize the transfer of the necessary parameters (index name, fields, etc.) from the other extensions (ExtensionConfiguration?) -> see in ElasticSearchServic
-// Elastic Search Index return standardized fields? Standardized search fields or own params from the respective extension?
-// process search parameters from the URL query parameters to search
+use TYPO3\CMS\Core\Page\AssetCollector;
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 
 final class SearchController extends ClientEnabledController
 {
     // set resultLimit as intern variable from $this->settings['resultLimit'];
     protected int $resultLimit;
+    private FrontendInterface $runtimeCache;
 
 
     // Dependency Injection of Repository
     // https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ApiOverview/DependencyInjection/Index.html#Dependency-Injection
-
     public function __construct(
         private readonly ElasticSearchServiceInterface $elasticSearchService,
         protected ExtensionConfiguration $extConf,
         private readonly PageTitleProvider $titleProvider,
-    )
-    {
+        private readonly AssetCollector $assetCollector,
+        CacheManager $cacheManager,
+    ) {
         $this->resultLimit = $this->settings['resultLimit'] ?? 25;
+        $this->runtimeCache = $cacheManager->getCache('runtime');
     }
 
     public function indexAction(array $searchParams = []): ResponseInterface
     {
-        $language = $this->request->getAttribute('language');
-        $locale = $language->getLocale();
-        if (
-            isset($searchParams['page']) &&
-            (int) $searchParams['page'] > 0
-        ) {
-            $currentPage = (int) $searchParams['page'];
-        } else {
-            $currentPage = 1;
-        }
-
-    //  $totalItems = $this->elasticSearchService->count($searchParams, $this->settings);
-        //$totalItems = 100;
+        $locale = $this->request->getAttribute('language')->getLocale();
+        $currentPage = $this->getCurrentPage($searchParams);
+        $this->addViewTransitionStyle();
 
         $elasticResponse = $this->elasticSearchService->search($searchParams, $this->settings);
-        $paginator = (new Paginator())->
-            setPage($currentPage)->
-            setTotalItems($elasticResponse['hits']['total']['value'])->
-            setExtensionConfiguration($this->extConf);
+        $totalItems = 0;
+        if (isset($elasticResponse['hits'], $elasticResponse['hits']['total'], $elasticResponse['hits']['total']['value'])) {
+            $totalItems = (int)$elasticResponse['hits']['total']['value'];
+        }
+
+        $paginator = (new Paginator())
+            ->setPage($currentPage)
+            ->setTotalItems($totalItems)
+            ->setExtensionConfiguration($this->extConf);
         $pagination = $paginator->getPagination();
-        $showPagination = $paginator->getTotalPages() > 1 ? true : false;
+        $showPagination = $paginator->getTotalPages() > 1;
 
-        $this->view->assign('locale', $locale);
-        $this->view->assign('totalItems', $elasticResponse['hits']['total']['value']);
-        $this->view->assign('searchParams', $searchParams);
-        $this->view->assign('searchResults', $elasticResponse);
-        $this->view->assign('pagination', $pagination);
-        $this->view->assign('showPagination', $showPagination);
-     //   $this->view->assign('totalItems', $totalItems);
-        $this->view->assign('currentString', Paginator::CURRENT_PAGE);
-        $this->view->assign('dots', Paginator::DOTS);
-        $this->view->assign('detailPageId', $this->extConf->get('liszt_common','detailPageId'));
-
+        $this->view->assignMultiple([
+            'locale'        => $locale,
+            'totalItems'    => $totalItems,
+            'searchParams'  => $searchParams,
+            'searchResults' => $elasticResponse,
+            'pagination'    => $pagination,
+            'showPagination'=> $showPagination,
+            'currentString' => Paginator::CURRENT_PAGE,
+            'dots'          => Paginator::DOTS,
+            'detailPageId'  => $this->extConf->get('liszt_common', 'detailPageId'),
+        ]);
 
         return $this->htmlResponse();
     }
+
 
     public function searchBarAction(array $searchParams = []): ResponseInterface
     {
@@ -82,22 +79,37 @@ final class SearchController extends ClientEnabledController
         return $this->htmlResponse();
     }
 
-    public function detailsAction(array $searchParams = []): ResponseInterface
+
+    public function detailsHeaderAction(array $searchParams = []): ResponseInterface
     {
-
-        $routing = $this->request->getAttribute('routing');
-        $routingArgs = $routing->getArguments();
-
-        // Check if 'tx_lisztcommon_searchdetails' exists and if 'detailId' has a valid value.
-        $documentId = null;
-        if (!empty($routingArgs['tx_lisztcommon_searchdetails']['documentId'])) {
-            $documentId = $routingArgs['tx_lisztcommon_searchdetails']['documentId'];
-        } else {
+        $documentId = $this->getDocumentIdFromRouting();
+        if (!$documentId) {
             return $this->redirectToNotFoundPage();
         }
 
         try {
-            $elasticResponse = $this->elasticSearchService->getDocumentById($documentId, []);
+            $elasticResponse = $this->loadDetailPageFromElastic($documentId);
+        } catch (ClientResponseException $e) {
+            if ($e->getCode() === 404) {
+                return $this->redirectToNotFoundPage();
+            }
+            throw $e;
+        }
+
+        $this->view->assign('searchResult', $elasticResponse);
+        return $this->htmlResponse();
+    }
+
+
+    public function detailsAction(array $searchParams = []): ResponseInterface
+    {
+        $documentId = $this->getDocumentIdFromRouting();
+        if (!$documentId) {
+            return $this->redirectToNotFoundPage();
+        }
+
+        try {
+            $elasticResponse = $this->loadDetailPageFromElastic($documentId);
         } catch (ClientResponseException $e) {
             // Handle 404 errors
             if ($e->getCode() === 404) {
@@ -107,54 +119,82 @@ final class SearchController extends ClientEnabledController
 
         }
 
-        // Set the page title
-        $pageTitle = $elasticResponse['_source']['title'] ?? 'Details';
-        $maxTitleLength = 50;
-        if (strlen($pageTitle) > $maxTitleLength) {
-            $pageTitle = mb_substr($pageTitle, 0, $maxTitleLength - 3) . '...';
-        }
+        // set page title
+        $pageTitle = $this->formatPageTitle($elasticResponse['_source']['title'] ?? 'Details');
         $this->titleProvider->setTitle($pageTitle);
-        // set metatag description
-        $metaTagManager = GeneralUtility::makeInstance(MetaTagManagerRegistry::class)->getManagerForProperty('description');
-        $metaTagManager->addProperty('description', $elasticResponse['_source']['tx_lisztcommon_searchable'] ?? '');
 
+        // set meta description
+        $metaDescription = '';
+        if (isset($elasticResponse['_source'], $elasticResponse['_source']['tx_lisztcommon_searchable'])) {
+            $metaDescription = (string)$elasticResponse['_source']['tx_lisztcommon_searchable'];
+        }
+        $metaTagManager = GeneralUtility::makeInstance(MetaTagManagerRegistry::class)
+            ->getManagerForProperty('description');
+        $metaTagManager->addProperty('description', $metaDescription);
 
-/*        $request = $this->request;
-        $debugData = [
-            'Method' => $request->getMethod(),
-            'URI' => (string)$request->getUri(),
-            'Headers' => $request->getHeaders(),
-            'QueryParams' => $request->getQueryParams(),
-            'ParsedBody' => $request->getParsedBody(),
-            'Attributes' => $request->getAttributes(),
-            'UploadedFiles' => $request->getUploadedFiles(),
-            'ServerParams' => $request->getServerParams()
-        ];
+        $this->addViewTransitionStyle();
 
-        // Ausgabe mit Symfony VarDumper (empfohlen)
-        dump($debugData);*/
-
-
-        $this->view->assign('searchParams', $searchParams);
-        $this->view->assign('routingArgs', $routingArgs);
-        $this->view->assign('detailId', $documentId);
-        $this->view->assign('searchResult', $elasticResponse);
-        $this->view->assign('detailPageId', $this->extConf->get('liszt_common','detailPageId'));
-
+        $this->view->assignMultiple([
+            'searchParams' => $searchParams,
+            'routingArgs'  => $this->request->getAttribute('routing')->getArguments(),
+            'detailId'     => $documentId,
+            'searchResult' => $elasticResponse,
+            'detailPageId' => $this->extConf->get('liszt_common', 'detailPageId'),
+        ]);
 
         return $this->htmlResponse();
-
-
     }
+
+    private function getCurrentPage(array $params): int
+    {
+        return (isset($params['page']) && (int)$params['page'] > 0) ? (int)$params['page'] : 1;
+    }
+
+
+    // add view transition styles as inline style for animation
+    private function addViewTransitionStyle(): void
+    {
+        $this->assetCollector->addInlineStyleSheet(
+            'view-transitions-root',
+            '@media screen and (prefers-reduced-motion: no-preference) { @view-transition { navigation: auto; } }'
+        );
+    }
+
+    private function getDocumentIdFromRouting(): ?string
+    {
+        $routingArgs = $this->request->getAttribute('routing')->getArguments();
+        return $routingArgs['tx_lisztcommon_searchdetails']['documentId'] ?? null;
+    }
+
+    private function formatPageTitle(string $title): string
+    {
+        $maxTitleLength = 50;
+        return (mb_strlen($title) > $maxTitleLength)
+            ? mb_substr($title, 0, $maxTitleLength - 3) . '...'
+            : $title;
+    }
+
+    private function loadDetailPageFromElastic(string $documentId): Collection
+    {
+        $cachedResult = $this->runtimeCache->get($documentId);
+        if (!$cachedResult instanceof Collection) {
+            $result = $this->elasticSearchService->getDocumentById($documentId, []);
+            if (!$result instanceof Collection) {
+                throw new \UnexpectedValueException("The return value of getDocumentById does not correspond to the expected collection type.");            }
+            $this->runtimeCache->set($documentId, $result);
+            return $result;
+        }
+        return $cachedResult;
+    }
+
 
 
     public function redirectToNotFoundPage(): ResponseInterface
     {
-    // see: https://docs.typo3.org/m/typo3/reference-coreapi/12.4/en-us/ExtensionArchitecture/Extbase/Reference/Controller/ActionController.html
+        // see: https://docs.typo3.org/m/typo3/reference-coreapi/12.4/en-us/ExtensionArchitecture/Extbase/Reference/Controller/ActionController.html
         // $uri could also be https://example.com/any/uri
         // $this->resourceFactory is injected as part of the `ActionController` inheritance
         return $this->responseFactory->createResponse(301)
             ->withHeader('Location', '/404/');   // the uri of the 404 page from typo installation
     }
-
 }
